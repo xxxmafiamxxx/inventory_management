a6f6cdb0336ffb9ed084366c56d01755aeeaca51
xxxmafiamxxx <mafia_a_1@hotmail.com>
2024-09-23 04:01:30 +0300

    fixed static files issues

--- inventory_management_system/inventory_management_system/settings.py
+++ inventory_management_system/inventory_management_system/settings.py
@@ -150,3 +150,22 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
